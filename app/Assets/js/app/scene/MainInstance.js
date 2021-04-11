17ffb9d11a205611447e1780fe93d6ecf457ef8c
Krists <kpudzens@gmail.com>
2021-04-11 11:17:32 +0300

    s

--- app/Assets/js/app/scene/MainInstance.js
+++ app/Assets/js/app/scene/MainInstance.js
@@ -1,5 +1,6 @@
 import SceneManager from '@app/scene/SceneManager';
 
+//All hail The SINGLETON!
 const MainSceneInstance = new SceneManager(document.getElementById('renderCanvas'));
 
 export default MainSceneInstance;
